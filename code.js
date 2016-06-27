205f2d600ee463ade7cc6073a31fe79a75bb5135
Noelle Rei <reidnoelle2@gmail.com>
2016-06-27 14:34:15 -0600

    added journal jun26

--- code.js
+++ code.js
@@ -1,1 +1,1 @@
-var journal = ["I wonder how I did on the assessment. That sandwich was GR8."];
+var journal = ["I wonder how I did on the assessment. That sandwich was GR8.", "Good thing I reviewed Git a bunch."];
