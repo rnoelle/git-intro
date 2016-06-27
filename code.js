3c36aa21755df1ed14461ee758c3e57d95cfc88c
rnoelle <reidnoelle2@gmail.com>
2016-06-27 14:44:37 -0600

    added comment for var nextChange

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
 var journal = ["I wonder how I did on the assessment. That sandwich was GR8.", "Good thing I reviewed Git a bunch."];
 /* I guess we can have a comment in here. */
 var nextChange = "this one too.";
+/* Which may require a second comment. */
