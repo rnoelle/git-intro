27079d0489d97922d1685729770f256d2e1c90f7
Noelle Rei <reidnoelle2@gmail.com>
2016-06-27 14:43:14 -0600

    added second variable

--- code.js
+++ code.js
@@ -1,2 +1,3 @@
 var journal = ["I wonder how I did on the assessment. That sandwich was GR8.", "Good thing I reviewed Git a bunch."];
 /* I guess we can have a comment in here. */
+var nextChange = "this one too.";
